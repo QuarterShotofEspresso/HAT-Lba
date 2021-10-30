51eb8259cfeef6a1dcf5eca54238a77e27f53441
QSoE <32082930+QuarterShotofEspresso@users.noreply.github.com>
2021-10-29 21:59:18 -0700

    moved setup to babai dir and drafting babai.py

--- python/babai/src/lattice.py
+++ python/babai/src/lattice.py
@@ -2,33 +2,49 @@ import numpy as np
 
 class Lattice:
     
-    # Private Data Members
-    pri_basis
-    pri_dimension
-
-
-    # INTERFACE METHODS
+    # ==================== META & INST METHODS ======================
     # accept dim, target/random
-    def __init__(self):
-        self.pri_basis
-        self.pri_dimension
+    def __init__(self, dimension):
+        self.pri_basis = 0
+        self.pri_dim = dimension
 
 
     def __repr__(self):
-        return "Lattice of dim: {}".format(self.pri_dimension)
+        return "Lattice of dim: {}".format(self.pri_dim)
 
 
-    # HELPER METHODS
+    # ==================== HELPER METHODS ======================
+    # project: project vector u onto vector v
+    # return type: a vector (numpy ndarray)
     @classmethod
-    def genApprOrthogBasis(self):
+    def project(u, v):
+        dot_uv = np.dot(u, v)
+        dot_uu = np.dot(u, u)
+        return (dot_uv / dot_uu) * u
+
+
+    # gram_schmidt: use the gram-schmidt method to create
+    # a basis
+    # return type: boolean (python True/False)
+    def gram_schmidt(self):
+        for v in self.pri_basis:
+            v - Lattice.project()
         
 
+    # ===================== USER METHODS =======================
+    # makebasis: generate a basis of dimension pri_dimension
+    def makebasis(self):
+        random_basis = np.round(np.rand(self.pri_dim, 1) * 9999)
+        for i in range(1, self.pri_dim):
 
-    # USER METHODS
-    def babai(self, target):
+    # babai: solve the CVP using babai's algorithm and target
+    # vector w and basis pri_basis
+    def babai(self, w):
         
 
-    def hadamard(self):
+    # hadamard: compute the hadamard ratio using of the basis
+    # pri_basis
+    def hadamard(self, ):
 
-
-    def solve_system():
+    # linear_solve: solve the linear system of equations
+    def linear_solve(self, ):
