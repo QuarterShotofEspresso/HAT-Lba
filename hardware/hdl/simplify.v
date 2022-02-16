b9f3be66668806ee45cde511de68222a4600742e
QSoE <32082930+QuarterShotofEspresso@users.noreply.github.com>
2022-02-15 16:02:18 -0800

    drafting simplify module

---
+++ hardware/hdl/simplify.v
@@ -0,0 +1,27 @@
+// Author: Ratnodeep Bandyopadhyay. Copyright 2022.
+// Simplify Module
+
+
+module turn_positive
+
+
+module simplify #(INT_WIDTH = 64, THRESH_INDEX = 30)
+    (
+        input [INT_WIDTH-1:0] in_num,
+        input [INT_WIDTH-1:0] in_den,
+        input clk,
+
+        output [INT_WIDTH-2:0] out_num,
+        output [INT_WIDTH-1:0] out_den
+    )
+
+    reg
+
+    
+
+    always @(posedge clk) begin
+        
+
+    end
+
+endmodule
