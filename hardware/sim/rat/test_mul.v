7f0ff5382f4f24e09cd58f4d278f84162995d58d
QSoE <32082930+QuarterShotofEspresso@users.noreply.github.com>
2022-03-06 04:35:20 -0800

    completed mul testing

--- hardware/sim/rat/test_mul.v
+++ hardware/sim/rat/test_mul.v
@@ -1,13 +1,12 @@
 `timescale 1 ns/ 10 ps
-//`include "hdl/rat/mul.v"
 
-
-module mul_tb;
+module div_tb;
     
+    // configuration integers
     parameter WIDTH=32;
-    parameter BOUND=1000;
-    parameter TOTAL_TESTS = 20;
-
+    integer BOUND=1000;
+    integer TOTAL_TESTS = 20;
+    integer DELAY = 3;
 
     reg [WIDTH-1:0] l_num;
     reg [WIDTH-1:0] l_den;
@@ -21,19 +20,29 @@ module mul_tb;
     mul #(WIDTH) dut (clk, l_num, l_den, r_num, r_den, s_num, s_den);
 
     integer i;
-    integer test_passed = TOTAL_TESTS;
+    integer test_passed = 0;
 
     initial begin
 
-
         // Add plusargs here if necessary
+        if ($value$plusargs("BOUND=%0d", BOUND)) begin
+            $display("Config param found. \'BOUND\' = %0d", BOUND);
+        end
+        if ($value$plusargs("TOTAL_TESTS=%0d", TOTAL_TESTS)) begin
+            $display("Config param found. \'TOTAL_TESTS\' = %0d", TOTAL_TESTS);
+        end
+        if ($value$plusargs("DELAY=%0d", DELAY)) begin
+            $display("Config param found. \'DELAY\' = %0d", DELAY);
+        end
+
+        test_passed = TOTAL_TESTS;
 
         for(i = 0; i < TOTAL_TESTS; ++i) begin
             l_num = $urandom % BOUND;
             l_den = $urandom % BOUND;
             r_num = $urandom % BOUND;
             r_den = $urandom % BOUND;
-            #3;
+            #DELAY;
             if(l_num * r_num !== s_num || l_den * r_den !== s_den) begin
                 $display("TEST FAILED:\t(%0d / %0d) * (%0d / %0d) = (%0d / %0d) != (%0d / %0d)", l_num, l_den, r_num, r_den, l_num * r_num, l_den * r_den, s_num, s_den);
                 test_passed--;
